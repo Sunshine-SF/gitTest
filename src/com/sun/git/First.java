1af173fe3a49b65c7f8f01243bfba2df25553f6a
renr <123444@aliyun.com>
2020-05-19 14:41:08 +0800

    change file

--- src/com/sun/git/First.java
+++ src/com/sun/git/First.java
@@ -3,6 +3,7 @@ package com.sun.git;
 public class First {
     public static void main(String[] args) {
         System.out.println("sunshine");
-        哈哈哈哈哈哈哈
+      // 哈哈哈哈哈哈哈
+        int a = 10 ;
     }
 }
