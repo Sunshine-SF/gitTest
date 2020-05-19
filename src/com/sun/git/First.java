8675b55d92f9ec14da946102885dc4cf32ec78b2
renr <123444@aliyun.com>
2020-05-19 10:42:06 +0800

    zadad

--- src/com/sun/git/First.java
+++ src/com/sun/git/First.java
@@ -3,5 +3,6 @@ package com.sun.git;
 public class First {
     public static void main(String[] args) {
         System.out.println("sunshine");
+        System.out.println("I Love Sunshine");
     }
 }
